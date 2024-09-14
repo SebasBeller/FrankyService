cc1dc79c95aeca2696a2ed2e3b55a8081e31174f
987sebasBeller <sebastianbeller76@gmail.com>
2024-09-13 23:39:33 -0400

    The Greeting Service was created

---
+++ src/main/java/com/example/FrankyService/GreetingService.java
@@ -0,0 +1,11 @@
+package com.example.FrankyService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    String greet="Pregúntate si lo que estás haciendo hoy te acerca al lugar en el que quieres estar mañana.";
+    public String getGreeting() {
+        return greet;
+    }
+}
